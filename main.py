02d63457391db7c345645bf3ccd9c60adfaa1896
mrafliramli <32848277-mrafliramli@users.noreply.replit.com>
2024-03-16 07:46:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# membuat variable
+nama = "Muhamad Rafli Ramli"
+semester = 2
+kelas = "TI23B"
+jurusan = "Teknik Informatika"
+quote = "Hidup seperti larry"
+married = False
+adalahPerempuan = False
+hobi = 'Volli, Main Game, Makan'
+
+print("Nama saya adalah ", nama)
+print("Semester ", semester)
+print("Kelas saya ", kelas)
+print("Jurusan saya ", jurusan)
+print("Quote ", quote)
+print("Hobi saya ", hobi)
